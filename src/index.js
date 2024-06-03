a07f4135b169d951363415bb83b19399a3bb6637
bethanchatwin <168184672+bethanchatwin@users.noreply.github.com>
2024-06-03 19:43:55 +0100

    added blink animation to heart

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function showAnswer(response) {
+function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -6,17 +6,22 @@ function showAnswer(response) {
     delay: 20,
   });
 }
-function displayPoem(event) {
+function generatePoem(event) {
   event.preventDefault();
+
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "0083118ae860436btf146b9cbfb3oc7f";
   let context =
     "you are a poetic romantic. please keep your response fairly short";
   let prompt = `User instructions: Generate a love poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "Generating a poem for you ... please wait.";
-  axios.get(apiUrl).then(showAnswer);
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a love poem about ${instructionsInput.value} <div class="heart">♥</div>`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
-let poemButton = document.querySelector("#button");
-poemButton.addEventListener("click", displayPoem);
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
